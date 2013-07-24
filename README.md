40171110b66c7efa06694d1d3aef25f41e46056d
Anthony Loop <theseattlite@gmail.com>
2013-07-24 07:04:22 -0700

    Separate some logic, add some runtime flags, update readme

--- README.md
+++ README.md
@@ -1,41 +1,52 @@
-# Simple Dotfile Management
+# Dotfile Management
 
-Just a fairly simple way to manage my dotfiles
+Just a way to manage my dotfiles
 
 ## Install
 
-1. Clone the repository and pull in all the submodules:
+1. Clone the repository and pull in any the submodules:
 
-   ```
-   git clone --recursive https://github.com/aloop/dotfiles.git ~/.dotfiles
-   ```
+        git clone --recursive https://github.com/aloop/dotfiles.git ~/.dotfiles
 
-2. And then run the install script (Make sure the default python interpreter is python 2)
+2. And then make the install script executable and run it
 
-   ```
-   python ~/.dotfiles/install.py
-   ```
+        chmod +x ~/.dotfiles/manage.py
+        ~/.dotfiles/manage.py install
 
-### Notes
-* I have not tested install.py with python 3, and I don't expect it would work.
-* It should be possible to clone this repo to just about any location, however
-  i've only tested it at ~/.dotfiles myself.
-* This is the first python script i've ever written, so a gremlin infestation is likely.
-* And on that last note, i'm not responsible if this eats your directory contents. It hasn't been so unkind to me however.
+## Backups
 
-Files/Folders to be linked/copied that already exist in the home directory will be moved to:
+Files and Folders to be linked or copied that already exist in the home directory will be backed up to:
 
-```
-~/.dotfiles/backups/HOSTNAME@YYYY-MM-DD-HHMMSS
-```
 
-## Usage
+    ~/.dotfiles/backups/HOSTNAME@YYYY-MM-DD-HHMMSS
 
-To re-link or add new links, simply run the install script again:
 
-```
-python ~/.dotfiles/install.py
-```
+This behavior can be disabled at runtime with the `--no-backup` flag.
+
+## Basic Usage
+
+To update all links and copies, simply run the install script again:
+
+    ~/.dotfiles/manage.py install
+
+On the other hand, you may wish to only update links *or* copies:
+
+    ~/.dotfiles/manage.py link
+    
+or:
+   
+    ~/.dotfiles/manage.py copy 
+
+Certain files or folders can be excluded from being linked or copied individually like so:
+
+    ~/.dotfiles/manage.py install --ignore FILENAME FOLDERNAME …
+
+Currently globs in these names are **not** supported.
+
+## Notes
+* I have only used this script with python versions 2.7 and 3.3 (OSX 10.8 native and via homebrew, and Ubuntu 13.04 server)
+* It should be possible to use this script in just about any directory, i've only tested it at ~/.dotfiles, however.
+* This is the first python script i've ever written, beware of gremlins!
 
 ## Author
 
---
+++ manage.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+"""Simple yet powerful dotfile management"""
+
+import os
+import socket
+import argparse
+import logging as log
+import filesystem as fs
+from datetime import datetime
+
+
+class Dotfiles(object):
+    """
+    Manages dotfiles by linking or copying them
+    to a users home directory and can optionally
+    backup existing files.
+    """
+    def __init__(self, dir_, ignore=None):
+        self.datetime = datetime.now().strftime("%Y-%m-%d-%H%M%S")
+        self.dir = dir_
+        self.home = os.path.expanduser('~')
+        self.ignore = ignore if ignore else []
+        self.make_backups = True
+
+    def process_dir(self, path, func):
+        path = os.path.join(self.dir, path)
+        for filename in fs.file_list(path, self.ignore):
+            source = os.path.join(self.dir, path, filename)
+            dest = os.path.join(self.home, filename)
+
+            if self.make_backups:
+                self.backup(dest)
+            fs.remove(dest)
+
+            func(source, dest)
+
+    def backup(self, path):
+        backup_name = "{0}@{1}".format(socket.getfqdn(), self.datetime)
+        backup_path = os.path.join(self.dir, "backups", backup_name)
+        filename = os.path.basename(path)
+
+        # Create the backup directory in case it doesn't exist already
+        fs.mkdir(backup_path)
+
+        fs.copy(path, os.path.join(backup_path, filename))
+
+    def install(self):
+        self.link()
+        self.copy()
+
+    def link(self):
+        self.process_dir("link", fs.link)
+
+    def copy(self):
+        self.process_dir("copy", fs.copy)
+
+
+def main():
+    default_ignore_list = [
+        '.git',
+        '.DS_Store'
+    ]
+
+    dotfiles_dir = os.path.dirname(os.path.realpath(__file__))
+
+    dotfiles_obj = Dotfiles(dotfiles_dir, default_ignore_list)
+
+    file_description = "A Simple way to manage your dotfiles."
+
+    parentp = argparse.ArgumentParser(add_help=False)
+
+    parentp.add_argument('-v', '--verbose', action='store_true',
+                         help='Show more verbose output')
+
+    parentp.add_argument('--no-backup', action='store_true',
+                         help=('Do not backup existing files '
+                               'from the home directory'))
+
+    parentp.add_argument('--ignore', metavar='FILENAME', action='append',
+                         nargs='+', help=('Specify one or more files '
+                                          'or folders to ignore'))
+
+    parser = argparse.ArgumentParser(parents=[parentp],
+                                     description=file_description)
+
+    subp = parser.add_subparsers(title='Commands', metavar='<command>')
+
+    subparser_definitions = [
+        [
+            'install',
+            'Perform an install of dotfiles',
+            dotfiles_obj.install
+        ],
+        [
+            'link',
+            'Update dotfile links',
+            dotfiles_obj.link
+        ],
+        [
+            'copy',
+            'Update dotfile copies',
+            dotfiles_obj.copy
+        ]
+    ]
+
+    for name, help, func in subparser_definitions:
+        new_subp = subp.add_parser(name, help=help, parents=[parentp])
+        new_subp.set_defaults(func=func)
+
+    options = parser.parse_args()
+
+    log_format = '%(levelname)s: %(message)s'
+    if options.verbose:
+        log.basicConfig(format=log_format, level=log.DEBUG)
+    else:
+        log.basicConfig(format=log_format, level=log.WARNING)
+
+    if options.no_backup:
+        dotfiles_obj.make_backups = False
+    if options.ignore:
+        for sublist in options.ignore:
+            dotfiles_obj.ignore.extend(sublist)
+
+    try:
+        options.func()
+    except Exception as e:
+        log.error(e)
+        parser.exit(1)
+    else:
+        print('Everything looks good! Enjoy.')
+
+
+if __name__ == "__main__":
+    main()
