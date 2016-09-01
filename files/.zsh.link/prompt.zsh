bbd974fbd4f6673d5b12ccb1b9c6e04c07c86d23
Anthony Loop <theseattlite@gmail.com>
2016-09-01 05:10:40 -0700

    Fix prompt on some versions of zsh, tweak git prompt

--- files/.zsh.link/prompt.zsh
+++ files/.zsh.link/prompt.zsh
@@ -1,3 +1,5 @@
+autoload -Uz colors && colors
+
 prompt_user_str=""
 
 if [ -n "$SSH_CONNECTION" ] || [ -n "$SSH_TTY" ] || [ $UID -eq 0 ]; then
@@ -6,6 +8,6 @@ fi
 
 return_code="%(?..%F{red}%? ↵%f)"
 
-PROMPT='${prompt_user_str}%F{green}%3~ %F{yellow}${vcs_info_msg_0_}%f%B%F{%(!.red.blue)}»%f%b '
+PROMPT='${prompt_user_str}%F{green}%3~%F{yellow}${vcs_info_msg_0_:- }%f%B%(!.%F{red}.%F{blue})»%f%b '
 
 RPS1="${return_code}"
