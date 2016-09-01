153c5fd315f45d9f821cf4ba0753a686ff50d80d
Anthony Loop <theseattlite@gmail.com>
2016-09-01 05:10:40 -0700

    Fix prompt on some versions of zsh, tweak git prompt

--- files/.zsh.link/vcs_info.zsh
+++ files/.zsh.link/vcs_info.zsh
@@ -1,10 +1,11 @@
 autoload -Uz vcs_info
 
 zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' get-revision true
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:git*' actionformats "<%b%m%u%c (%a)> "
-zstyle ':vcs_info:git*' formats "<%b%m%u%c> "
-zstyle ':vcs_info:git*' stagedstr "*"
+zstyle ':vcs_info:git*' actionformats " <%b%m%u%c (%F{red}%a%f)> "
+zstyle ':vcs_info:git*' formats " <%b%m%u%c> "
+zstyle ':vcs_info:git*' stagedstr "+"
 zstyle ':vcs_info:git*' unstagedstr "*"
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
@@ -15,7 +16,7 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
         [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ] &&
         git status --porcelain | grep '??' &> /dev/null
     then
-        zstyle -s ':vcs_info:git*' stagedstr untrackedstr
+        zstyle -s ':vcs_info:git*' unstagedstr untrackedstr
         hook_com[unstaged]=$untrackedstr
     fi
 }
