d7fbbcd3d5dbfbf085ac762020421976924c35b6
Anthony Loop <anthony@anthonyloop.com>
2018-08-16 15:09:58 -0700

    Fix prompt on when using ssh

--- files/.shell-config.link/zsh/prompt.zsh
+++ files/.shell-config.link/zsh/prompt.zsh
@@ -2,15 +2,14 @@ autoload -Uz colors && colors
 
 dotfiles_prompt_setup() {
     local prompt_symbol="❯"
-    local prompt_user_str=""
+
+    PROMPT=""
 
     if [ -n "$SSH_CONNECTION" ] || [ -n "$SSH_TTY" ] || [ $UID -eq 0 ]; then
-        local prompt_user_str="%n%F{blue}@%f%M %B%F{blue}::%f%b "
+        PROMPT="%n%F{blue}@%f%M %B%F{blue}::%f%b "
     fi
 
-    PROMPT='${prompt_user_str}%F{green}%3~%F{yellow}${vcs_info_msg_0_:- }%f%B%(?.%F{blue}.%F{red})'
-    PROMPT+="${prompt_symbol}"
-    PROMPT+='%f%b '
+    PROMPT+='${prompt_user_str}%F{green}%3~%F{yellow}${vcs_info_msg_0_:- }%f%B%(?.%F{blue}.%F{red})'"${prompt_symbol}"'%f%b '
 }
 
 dotfiles_prompt_setup
