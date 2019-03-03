472baa119133de53e0788c61a05d351633cd7ab6
Anthony Loop <anthony@anthonyloop.com>
2019-03-02 18:35:21 -0800

    zsh: fix some bugs with the new prompt, show short hash for git repos

--- shell/zsh/vcs-info.zsh
+++ shell/zsh/vcs-info.zsh
@@ -6,27 +6,32 @@ dotfiles_setup_vcs_info() {
 
     zstyle ':vcs_info:*' enable git
     zstyle ':vcs_info:*' get-revision true
-    zstyle ':vcs_info:*' check-for-changes true
-    zstyle ':vcs_info:git*' actionformats " %{%F{white}%}on%{%f%} %{%F{blue}%}%b%{%f%} %m%{%F{red}%}%u%c%{%f%} (%{%F{red}%}%a%{%f%}) "
-    zstyle ':vcs_info:git*' formats " %{%F{white}%}on%{%f%} %{%F{blue}%}%b%{%f%} %m%{%F{red}%}%u%c%{%f%} "
-    zstyle ':vcs_info:git*' stagedstr $PROMPT_VCS_DIRTY_SYMBOL
-    zstyle ':vcs_info:git*' unstagedstr $PROMPT_VCS_DIRTY_SYMBOL
-    zstyle ':vcs_info:git*+set-message:*' hooks is-git-dirty
+    zstyle ':vcs_info:git*' actionformats " %{%F{white}%}on%{%f%} (%{%F{blue}%}%b%i%{%f%}) %m %u (%{%F{red}%}%a%{%f%}) "
+    zstyle ':vcs_info:git*' formats ' %{%F{white}%}on%{%f%} (%{%F{blue}%}%b%i%{%f%}) %m %u '
+    zstyle ':vcs_info:git*set-message:*' hooks git-output-symbols git-short-revision-hash
 
-    +vi-is-git-dirty() {
+    +vi-git-short-revision-hash() {
+        if [ -n "${hook_com[revision]}" ]; then
+            hook_com[revision]="%{%F{white}%}:%{%F{blue}%}${hook_com[revision]:0:12}"
+        fi
+    }
+
+    +vi-git-output-symbols() {
         if
-            [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] &&
-            git diff --quiet --ignore-submodules HEAD &>/dev/null
+            [[ $(command git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] &&
+            command git diff --no-ext-diff --quiet --exit-code
         then
-            # Hijack the staged hook output
-            hook_com[staged]="%{%B%}%{%F{green}%}${PROMPT_VCS_CLEAN_SYMBOL}%{%f%}%{%b%}"
+            # Hijack the unstaged hook output
+            hook_com[unstaged]="%{%B%}%{%F{green}%}${PROMPT_VCS_CLEAN_SYMBOL}%{%f%}%{%b%}"
+        else
+            hook_com[unstaged]="%{%B%}%{%F{red}%}${PROMPT_VCS_DIRTY_SYMBOL}%{%f%}%{%b%}"
         fi
     }
 
     vcs_info_precmd() {
         vcs_info
         # Perform some extra formatting and cleanup
-        vcs_info_msg_0_="$(printf "%s" "${vcs_info_msg_0_:- }" | tr -s ' ' | sed -e "s/${PROMPT_VCS_DIRTY_SYMBOL}\{2,\}/$PROMPT_VCS_DIRTY_SYMBOL/")"
+        vcs_info_msg_0_="$(printf "%s" "${vcs_info_msg_0_:- }" | tr -s ' ')"
     }
 
     precmd_functions+=vcs_info_precmd
