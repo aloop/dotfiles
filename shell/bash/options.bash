33c207f6d9faf4de3bfc86775d447fb9fbe47581
Anthony Loop <anthony@anthonyloop.com>
2019-03-07 22:19:02 -0800

    bash: set some bash-specific exports and options

---
+++ shell/bash/options.bash
@@ -0,0 +1,11 @@
+shopt -s histappend
+shopt -s cmdhist
+shopt -s checkwinsize
+shopt -s interactive_comments
+# Change directory if nothing but a directory name is given as a command
+shopt -s autocd 2>/dev/null
+shopt -s dirspell 2>/dev/null
+shopt -s cdspell 2>/dev/null
+# Enable **-style globbing
+shopt -s globstar 2>/dev/null
+shopt -s nocaseglob
