59d56036d1a243eaf94a4ae3051983ce1345be87
Anthony Loop <anthony@anthonyloop.com>
2019-09-13 17:44:38 -0700

    bash: fix HISTFILESIZE typo

--- shell/bash/exports.bash
+++ shell/bash/exports.bash
@@ -2,6 +2,6 @@
 export HISTFILE="${HOME}/.bash_history"
 export HISTCONTROL="erasedups:ignoreboth"
 export HISTSIZE=1000
-export HISTFILESEIZE=10000
+export HISTFILESIZE=10000
 # Trim long directory paths in the prompt
 PROMPT_DIRTRIM=2
