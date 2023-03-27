93638ae5f908ded8f9bd8adfa05e78c8a29a9739
Anthony Loop <anthony@anthonyloop.com>
2023-03-26 21:22:23 -0700

    zsh: new prompt theme

--- shell/zsh/vcs-info.zsh
+++ shell/zsh/vcs-info.zsh
@@ -1,22 +1,33 @@
 autoload -Uz vcs_info
 
 dotfiles_setup_vcs_info() {
-    PROMPT_VCS_CLEAN_SYMBOL='✓'
-    PROMPT_VCS_DIRTY_SYMBOL='✗'
-    PROMPT_VCS_STAGED_SYMBOL='✚'
-    PROMPT_VCS_UNTRACKED_SYMBOL='✭'
+    PROMPT_VCS_CLEAN_SYMBOL="✓"
+    PROMPT_VCS_DIRTY_SYMBOL="✗"
+    PROMPT_VCS_STAGED_SYMBOL="✚"
+    PROMPT_VCS_UNTRACKED_SYMBOL="✭"
+    PROMPT_VCS_COLOR_TEXT="#f5e0dc"
+    PROMPT_VCS_COLOR_BRANCH_REV="#cba6f7"
+    PROMPT_VCS_COLOR_UNTRACKED="#f9e2af"
+    PROMPT_VCS_COLOR_STAGED="#a6e3a1"
+    PROMPT_VCS_COLOR_DIRTY="#f38ba8"
+    PROMPT_VCS_COLOR_CLEAN="#a6e3a1"
+    PROMPT_VCS_COLOR_ACTION="#cba6f7"
+
+    local vcs_format="%{%F{${PROMPT_VCS_COLOR_TEXT}}%}(%{%F{${PROMPT_VCS_COLOR_BRANCH_REV}}%}%b%i%{%F{${PROMPT_VCS_COLOR_TEXT}}%}) %u%c%m"
+    local vcs_action_format="${vcs_format} (%{%F{${PROMPT_VCS_COLOR_ACTION}}%}%a%{%f%})"
 
     zstyle ':vcs_info:*' enable git
     zstyle ':vcs_info:*' get-revision true
     zstyle ':vcs_info:*' check-for-changes true
-    zstyle ':vcs_info:*' stagedstr " %{%F{yellow}%}${PROMPT_VCS_STAGED_SYMBOL}%{%f%}"
-    zstyle ':vcs_info:git*' actionformats " %{%F{white}%}on%{%f%} (%{%F{blue}%}%b%i%{%f%}) %u%c%m (%{%F{red}%}%a%{%f%}) "
-    zstyle ':vcs_info:git*' formats ' %{%F{white}%}on%{%f%} (%{%F{blue}%}%b%i%{%f%}) %u%c%m '
-    zstyle ':vcs_info:git*set-message:*' hooks git-output-symbols git-untracked git-short-revision-hash
+    zstyle ':vcs_info:*' unstagedstr " %F{${PROMPT_VCS_COLOR_DIRTY}}${PROMPT_VCS_DIRTY_SYMBOL}%f"
+    zstyle ':vcs_info:*' stagedstr " %{%F{${PROMPT_VCS_COLOR_STAGED}}%}${PROMPT_VCS_STAGED_SYMBOL}%{%f%}"
+    zstyle ':vcs_info:git*' formats " ${vcs_format} "
+    zstyle ':vcs_info:git*' actionformats " ${vcs_action_format} "
+    zstyle ':vcs_info:git*+set-message:*' hooks git-output-symbols git-untracked git-short-revision-hash
 
     +vi-git-short-revision-hash() {
         if [ -n "${hook_com[revision]}" ]; then
-            hook_com[revision]="%{%F{white}%}:%{%F{blue}%}${hook_com[revision]:0:12}"
+            hook_com[revision]="%{%F{${PROMPT_VCS_COLOR_TEXT}}%}:%{%F{${PROMPT_VCS_COLOR_BRANCH_REV}}%}${hook_com[revision]:0:12}"
         fi
     }
 
@@ -26,9 +37,9 @@ dotfiles_setup_vcs_info() {
             command git diff --no-ext-diff --quiet --exit-code &>/dev/null
         then
             # Hijack the unstaged hook output
-            hook_com[unstaged]="%{%B%}%{%F{green}%}${PROMPT_VCS_CLEAN_SYMBOL}%{%f%}%{%b%}"
+            hook_com[unstaged]="%{%B%}%{%F{${PROMPT_VCS_COLOR_CLEAN}}%}${PROMPT_VCS_CLEAN_SYMBOL}%{%f%}%{%b%}"
         else
-            hook_com[unstaged]="%{%B%}%{%F{red}%}${PROMPT_VCS_DIRTY_SYMBOL}%{%f%}%{%b%}"
+            hook_com[unstaged]="%{%B%}%{%F{${PROMPT_VCS_COLOR_DIRTY}}%}${PROMPT_VCS_DIRTY_SYMBOL}%{%f%}%{%b%}"
         fi
     }
 
@@ -36,14 +47,17 @@ dotfiles_setup_vcs_info() {
         if [[ $(command git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
             command git status --porcelain | grep -m 1 '^??' &>/dev/null
         then
-            hook_com[misc]+=" %{%B%}%{%F{cyan}%}${PROMPT_VCS_UNTRACKED_SYMBOL}%{%f%}%{%b%}"
+            hook_com[misc]+=" %{%B%}%{%F{${PROMPT_VCS_COLOR_UNTRACKED}}%}${PROMPT_VCS_UNTRACKED_SYMBOL}%{%f%}%{%b%}"
         fi
     }
 
     vcs_info_precmd() {
         vcs_info
+
         # Condense consecutive spaces into a single space
-        vcs_info_msg_0_="$(printf "%s" "${vcs_info_msg_0_:- }" | tr -s ' ')"
+        while [ "${vcs_info_msg_0_//  / }" != "$vcs_info_msg_0_" ]; do
+            vcs_info_msg_0_="${vcs_info_msg_0_//  / }"
+        done
     }
 
     precmd_functions+=vcs_info_precmd
@@ -52,4 +66,3 @@ dotfiles_setup_vcs_info() {
 dotfiles_setup_vcs_info
 
 unset dotfiles_setup_vcs_info
-
