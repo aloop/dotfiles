56532c8fb3683a4709b251a2234281054ea79a34
Anthony Loop <anthony@anthonyloop.com>
2023-03-27 00:53:51 -0700

    zsh: remove zplug, use simple custom plugin manager

--- files-env/XDG_CONFIG_HOME/zsh.link/.zshrc
+++ files-env/XDG_CONFIG_HOME/zsh.link/.zshrc
@@ -11,44 +11,6 @@ if [ ! -d "${XDG_STATE_HOME}/zsh" ]; then
     mkdir -p "${XDG_STATE_HOME}/zsh"
 fi
 
-# Install zplug if it doesn't exist
-if [ ! -d "${ZPLUG_HOME}" ]; then
-    git clone https://github.com/zplug/zplug "${ZPLUG_HOME}" >/dev/null
-fi
-
-# Start loading plugins
-if [ -f "${ZPLUG_HOME}/init.zsh" ]; then
-    . "${ZPLUG_HOME}/init.zsh"
-
-    zplug "zplug/zplug", hook-build:"zplug --self-manage"
-
-    # Add async worker support
-    zplug "mafredri/zsh-async", from:"github", use:"async.zsh"
-
-    # inline suggestions
-    zplug "zsh-users/zsh-autosuggestions"
-
-    # completions
-    zplug "zsh-users/zsh-completions"
-
-    # Allow a user to declare plugins for an individual install
-    if [ -f "${ZDOTDIR}/local/plugins.zsh" ]; then
-        . "${ZDOTDIR}/local/plugins.zsh"
-    fi
-
-    # Syntax highlighting - apparently needs be last
-    zplug "zsh-users/zsh-syntax-highlighting", defer:2
-
-    # zplug check returns true if all packages are installed
-    # Therefore, when it returns false, run zplug install
-    if ! zplug check; then
-        zplug install
-    fi
-
-    # source plugins and add commands to the PATH
-    zplug load
-fi
-
 # Source scripts intended for an interactive shell
 . "${DOTFILES_DIR}/shell/interactive.sh"
 
---
+++ shell/zsh/80-plugin-manager.zsh
@@ -0,0 +1,38 @@
+# Ensure that $DOTFILES_ZSH_PLUGINS_DIR is set
+if [ -z "${DOTFILES_ZSH_PLUGINS_DIR}" ]; then
+    return 0
+fi
+
+# Ensure that $DOTFILES_ZSH_PLUGINS_DIR exists
+if [ ! -d "${DOTFILES_ZSH_PLUGINS_DIR}" ]; then
+    mkdir -p "${DOTFILES_ZSH_PLUGINS_DIR}"
+fi
+
+dotfiles_plugin() {
+    local plugin_url="$1"
+    # Operate under the convention of having a script named
+    # the same as the repo, with an added .plugin.zsh extension
+    #
+    # For example: zsh-users/zsh-completions => zsh-completions.plugin.zsh
+    local plugin_script_name="${2:-"$(basename "${1}").plugin.zsh"}"
+    local repo_name="${3:-"$(basename "${1}")"}"
+    local plugin_source_path="${DOTFILES_ZSH_PLUGINS_DIR}/${repo_name}/${plugin_script_name}"
+
+    if [ -z "$plugin_url" ]; then
+        return 0
+    fi
+
+    if [ ! -d "${DOTFILES_ZSH_PLUGINS_DIR}/${repo_name}" ]; then
+        command git clone "${plugin_url}" "${DOTFILES_ZSH_PLUGINS_DIR}/${repo_name}"
+    fi
+
+    if [ -f "${plugin_source_path}" ]; then
+        . "${plugin_source_path}"
+    fi
+}
+
+dotfiles_plugin "https://github.com/mafredri/zsh-async" "async.zsh"
+dotfiles_plugin "https://github.com/zsh-users/zsh-autosuggestions"
+dotfiles_plugin "https://github.com/zsh-users/zsh-completions"
+dotfiles_plugin "https://github.com/zsh-users/zsh-completions"
+dotfiles_plugin "https://github.com/zsh-users/zsh-syntax-highlighting"
