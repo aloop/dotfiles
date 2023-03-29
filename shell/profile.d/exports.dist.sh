169ddf6393eba87c85c585748eb9b1794f373bc7
Anthony Loop <anthony@anthonyloop.com>
2023-03-29 16:07:36 -0700

    shell: split xdg base dir definitions into their own file

--- shell/profile.d/exports.dist.sh
+++ shell/profile.d/exports.dist.sh
@@ -11,18 +11,6 @@ fi
 # Setup the golang path if it isn't already set
 export GOPATH=${GOPATH:-"${HOME}/.go"}
 
-# Setup defaults for XDG
-export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
-export XDG_CACHE_HOME="${XDG_CACHE_HOME:-"${HOME}/.cache"}"
-export XDG_STATE_HOME="${XDG_STATE_HOME:-"${HOME}/.local/state"}"
-export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-"${HOME}/.run"}"
-export XDG_DATA_HOME="${XDG_DATA_HOME:-"${HOME}/.local/share"}"
-
-# Add snapd dir to XDG_DATA_DIRS
-if [ "${XDG_DATA_DIRS#*/snapd/desktop}" = "${XDG_DATA_DIRS}" ]; then
-    export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}:/var/lib/snapd/desktop"
-fi
-
 export GNUPGHOME="${GNUPGHOME:-"${XDG_CONFIG_HOME}/gnupg"}"
 
 export PAGER="${PAGER:-"less"}"
@@ -46,4 +34,3 @@ export QT_QPA_PLATFORM="wayland;xcb"
 if [ -n "$WT_SESSION" ]; then
     export COLORTERM="truecolor"
 fi
-
