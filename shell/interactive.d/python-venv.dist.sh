cac14828f2166d5e15a54bd2f76e12545bfd619d
Anthony Loop <anthony@anthonyloop.com>
2023-05-07 14:08:21 -0700

    shell: fix venv script

--- shell/interactive.d/python-venv.dist.sh
+++ shell/interactive.d/python-venv.dist.sh
@@ -7,13 +7,11 @@
 if [ -f "${XDG_DATA_HOME}/virtualenvs/dotfiles/bin/activate" ]; then
     . "${XDG_DATA_HOME}/virtualenvs/dotfiles/bin/activate"
 else
-    if df_command_exists python; then
+    if df_command_exists python3; then
         mkdir -p "${XDG_DATA_HOME}/virtualenvs"
-        if python -m venv --clear -- "${XDG_DATA_HOME}/virtualenvs/dotfiles" &>/dev/null; then
-            . "${XDG_DATA_HOME}/virtualenvs/dotfiles/bin/activate"
-            # Update pip and install the black formatter by default
-            pip install --upgrade pip black
+        if python3 -m venv --clear -- "${XDG_DATA_HOME}/virtualenvs/dotfiles" &>/dev/null; then
+            . "${XDG_DATA_HOME}/virtualenvs/dotfiles/bin/activate" && \
+            pip3 install --upgrade pip black
         fi
     fi
 fi
-
